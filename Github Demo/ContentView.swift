a4a81f7693f077a91d0dee612f6d13fff7ece601
Rajeev <rajeevjain@gmail.com>
2022-03-19 10:24:58 +0530

    Initial Commit

---
+++ Github Demo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Github Demo
+//
+//  Created by Rajeev Jain on 19/03/22.
+//
+
+import SwiftUI
+
+//add some comments
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world 1!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Github Demo/Github_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_DemoApp.swift
+//  Github Demo
+//
+//  Created by Rajeev Jain on 19/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Github_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
